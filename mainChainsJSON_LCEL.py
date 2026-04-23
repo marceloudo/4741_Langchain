4e7e928beb9eb759c5a27028c0a5de262db13f2a
Udo <marceloudo@gmail.com>
2026-04-22 22:29:27 -0300

    feat: Aula2 - finaliza mainChainsJSON_LCEL.py com lógica LCEL

--- mainChainsJSON_LCEL.py
+++ mainChainsJSON_LCEL.py
@@ -17,7 +17,12 @@ class Destino(BaseModel):
     cidade: str = Field("A cidade recomendada para visitar.")
     motivo: str = Field("O motivo pelo qual a cidade é recomendada.")
 
-parseador = JsonOutputParser(pydantic_object=Destino) # Criando um parseador JSON que utiliza o modelo de dados Destino.
+class Restaurantes(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar.")
+    restaurantes: str = Field("Os restaurantes recomendados na cidade.")
+
+parseador_destino = JsonOutputParser(pydantic_object=Destino) # Criando um parseador JSON que utiliza o modelo de dados Destino.
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restaurantes) # Criando um parseador JSON que utiliza o modelo de dados Restaurantes.
 
 prompt_cidade = PromptTemplate(
     template= """
@@ -25,9 +30,21 @@ prompt_cidade = PromptTemplate(
     {formato_de_saida}
     """,
     input_variables=["interesse"],
-    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
+    partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()}
 )
 
+prompt_restauantes = PromptTemplate(
+    template= """
+    Sugira restaurantes populares entre os locais em {cidade}.
+    {formato_de_saida}
+    """,
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador_restaurantes.get_format_instructions()}
+)
+
+prompt_cultural = PromptTemplate(
+    template="Sugira atividades e locais culturais em {cidade}",
+)
 
 modelo = ChatOpenAI(
     model="gpt-3.5-turbo",
@@ -35,7 +52,11 @@ modelo = ChatOpenAI(
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | parseador # Criando a cadeia de processamento, onde o prompt é processado pelo modelo e depois o resultado é parseado para o formato JSON definido pelo modelo de dados Destino.
+cadeia_1 = prompt_cidade | modelo | parseador_destino # Criando a cadeia de processamento, onde o prompt é processado pelo modelo e depois o resultado é parseado para o formato JSON definido pelo modelo de dados Destino.
+cadeia_2 = prompt_restauantes | modelo | parseador_restaurantes # Criando a cadeia de processamento, onde o prompt é processado pelo modelo e depois o resultado é parseado para o formato JSON definido pelo modelo de dados Restaurantes.
+cadeia_3 = prompt_cultural | modelo | StrOutputParser()
+
+cadeia = cadeia_1 | cadeia_2 | cadeia_3 # Combinando as três cadeias em uma única cadeia de processamento.
 
 resposta = cadeia.invoke(
     {
